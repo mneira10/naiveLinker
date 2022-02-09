c6f8175354f9d2ec9c231abee9fbc5d55ba3be7b
Mauricio Neira <mauro@up.codes>
2022-02-09 15:35:23 -0800

    Function cleanup

--- main.py
+++ main.py
@@ -6,63 +6,70 @@ from bs4 import BeautifulSoup
 
 def autolink_file(file_path):
 
-    parsed_html = loadHTML(args.file_path)
+    soup = loadHTML(args.file_path)
 
-    section_bodies = parsed_html.find_all("div", {"class": "section_body"})
+    find_and_insert_links(soup)
+
+    print(soup.prettify())
+
+def find_and_insert_links(soup):
+
+    section_bodies = soup.find_all("div", {"class": "section_body"})
 
     for section_body in section_bodies:
 
         for text in section_body.findAll(text=True):
-            if not str.isspace(text):
 
-                matches_iter = re.finditer(r'Section [0-9]+(\.[0-9]+)*', text)
-                matches = [match for match in matches_iter]
+            find_and_insert_links_in_text(soup, text)
+            
 
-                if len(matches) > 0:
-                    text_parent = text.parent
+def find_and_insert_links_in_text(soup, text):
+    
+    # a lot of text found by beautiful soup is blank text
+    if not str.isspace(text):
+
+        # This is a first approach - it will only get sections of the type:
+        # Section <section_number>
+        # Next iteration of this would be to also get Sections of the type:
+        # Sections 1209.2.1 through/and/or 1209.2.4
+        matches_iter = re.finditer(r'Section [A-Z]?[0-9]+(\.[0-9]+)*', text)
+        matches = list(matches_iter)
+
+        if len(matches) > 0:
+
+            last_match_end = 0 
+            raw_text = text.string
+            previous_link = None
+
+            for match in matches:
+                match_start, match_end = match.span()
+                match_content = match.group()
+                section_number = match_content.lstrip('Section ')
+
+                text_before_link = soup.new_string(raw_text[last_match_end:match_start])
+
+                new_link = soup.new_tag('a')
+                new_link.attrs['href'] = "#{}".format(section_number)
+                new_link.string = match_content
+
+                if last_match_end == 0:
+                    text.replace_with(text_before_link)
+                else:
+                    previous_link.insert_after(text_before_link)
+
+                text_before_link.insert_after(new_link)
+                previous_link = new_link
+
+                last_match_end = match_end
 
 
-                    print('#'*20)
-                    print('NUM MATCHES', len(matches) )
-                    print('PREV', text_parent)
-                    last_match_end = 0 
-                    raw_text = text.string
 
-                    previous_link = None
-                    for match in matches:
-                        print('the match:', match)
-                        match_start, match_end = match.span()
-                        match_content = match.group()
+            text_after_link = soup.new_string(raw_text[last_match_end:])
 
-                        section_number = match_content.split(' ')[1]
+            previous_link.insert_after(text_after_link)
 
-                        previous_text_tag = parsed_html.new_string(raw_text[last_match_end:match_start])
-
-                        new_link = parsed_html.new_tag('a')
-                        new_link.attrs['href'] = "#{}".format(section_number)
-                        new_link.string = match_content
-
-                        if last_match_end == 0:
-                            text.replace_with(previous_text_tag)
-
-                        else:
-                            previous_link.insert_after(previous_text_tag)
-
-                        previous_text_tag.insert_after(new_link)
-                        previous_link = new_link
-
-                        last_match_end = match_end
-                    print('AFTER', text_parent)
-                
-                    # print(text)
-                    # print('-----')
-                # else:
-                #     print('$'*10, 'No subs found')
-# 
-    # print(parsed_html.prettify())
 
 def loadHTML(file_path):
-    print('Parsing', file_path)
 
     with open(file_path) as f:
         file_content = f.read()
